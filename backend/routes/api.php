23aa0e3c36bca18da4fcee7732aca60a72e4082d
Diogo Santos <diogo.santos@ikatec.com.br>
2025-08-23 09:07:01 -0400

    feat: add sanctum config and authController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
@@ -20,6 +21,11 @@ use App\Http\Controllers\ProductController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->prefix('clients')->group(function () {
     Route::get('/', [ClientController::class, 'index']);
