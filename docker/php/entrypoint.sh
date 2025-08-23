92a20f16acc759816782c870850e05892a4f2f90
Diogo Santos <diogo.santos@ikatec.com.br>
2025-08-23 07:40:30 -0400

    refactor: remove frontend folder and add laravel project structure

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Ajusta permissões para storage e bootstrap/cache
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+
+# Espera o banco de dados MySQL ficar disponível
+echo "Waiting for MySQL to be available..."
+
+sleep 10
+
+echo "MySQL is up - running migrations and seeders..."
+
+php artisan migrate:fresh --seed --force
+
+if [ ! -L /var/www/public/storage ]; then
+  php artisan storage:link
+fi
+
+# Executa o comando padrão do container (php-fpm)
+exec "$@"
